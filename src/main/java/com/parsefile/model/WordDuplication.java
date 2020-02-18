23a7cf3d1e96a95e0b86ee10602d9b46a24efa3d
AdminPC\Admin <admin2k7@gmail.com>
2020-02-18 23:44:40 +0200

    Added WordDuplication class

---
+++ src/main/java/com/parsefile/model/WordDuplication.java
@@ -0,0 +1,22 @@
+package com.parsefile.model;
+
+public class WordDuplication {
+        private String duplicate;
+        private int quantity;
+
+    public String getDuplicate() {
+        return duplicate;
+    }
+
+    public void setDuplicate(String duplicate) {
+        this.duplicate = duplicate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
