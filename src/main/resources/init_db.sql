5231ce9eab4ed49c69c0573d0301e303d975aa56
AdminPC\Admin <admin2k7@gmail.com>
2020-02-21 18:47:28 +0200

    init_db updated

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -5,70 +5,72 @@ SET NAMES 'utf8';
 DROP DATABASE IF EXISTS sql3324004;
 
 CREATE DATABASE IF NOT EXISTS sql3324004
-CHARACTER SET utf8
-COLLATE utf8_unicode_ci;
+    CHARACTER SET utf8
+    COLLATE utf8_unicode_ci;
 
 USE sql3324004;
 
 CREATE TABLE IF NOT EXISTS `lines` (
-  line_id int(11) NOT NULL,
-  content longtext DEFAULT NULL,
-  PRIMARY KEY (line_id)
+                                       line_id int(11) NOT NULL AUTO_INCREMENT,
+                                       content longtext DEFAULT NULL,
+                                       PRIMARY KEY (line_id)
 )
-ENGINE = INNODB,
-CHARACTER SET utf8,
-COLLATE utf8_unicode_ci;
+    ENGINE = INNODB,
+    AUTO_INCREMENT = 2,
+    AVG_ROW_LENGTH = 16384,
+    CHARACTER SET utf8,
+    COLLATE utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS statistics (
-  statistics_id int(11) NOT NULL,
-  longest_word varchar(128) DEFAULT NULL,
-  shortest_word varchar(128) DEFAULT NULL,
-  line_length int(11) DEFAULT NULL,
-  words_quantity int(11) DEFAULT NULL,
-  average_word_length int(11) DEFAULT NULL,
-  non_space_symbol_quantity int(11) DEFAULT NULL,
-  PRIMARY KEY (statistics_id)
+                                          statistics_id int(11) NOT NULL AUTO_INCREMENT,
+                                          longest_word varchar(128) DEFAULT NULL,
+                                          shortest_word varchar(128) DEFAULT NULL,
+                                          line_length int(11) DEFAULT NULL,
+                                          words_quantity int(11) DEFAULT NULL,
+                                          average_word_length int(11) DEFAULT NULL,
+                                          non_space_symbol_quantity int(11) DEFAULT NULL,
+                                          PRIMARY KEY (statistics_id)
 )
-ENGINE = INNODB,
-CHARACTER SET utf8,
-COLLATE utf8_unicode_ci;
+    ENGINE = INNODB,
+    CHARACTER SET utf8,
+    COLLATE utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS lines_statistics (
-  line_id int(11) NOT NULL,
-  statistics_id int(11) NOT NULL,
-  PRIMARY KEY (line_id, statistics_id)
+                                                line_id int(11) NOT NULL,
+                                                statistics_id int(11) NOT NULL,
+                                                PRIMARY KEY (line_id, statistics_id)
 )
-ENGINE = INNODB,
-CHARACTER SET utf8,
-COLLATE utf8_unicode_ci;
+    ENGINE = INNODB,
+    CHARACTER SET utf8,
+    COLLATE utf8_unicode_ci;
 
 ALTER TABLE lines_statistics
-ADD INDEX statistics_fk_idx (statistics_id);
+    ADD INDEX statistics_fk_idx (statistics_id);
 
 ALTER TABLE lines_statistics
-ADD CONSTRAINT line_fk FOREIGN KEY (line_id)
-REFERENCES `lines` (line_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+    ADD CONSTRAINT line_fk FOREIGN KEY (line_id)
+        REFERENCES `lines` (line_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE lines_statistics
-ADD CONSTRAINT statistics_fk FOREIGN KEY (statistics_id)
-REFERENCES statistics (statistics_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+    ADD CONSTRAINT statistics_fk FOREIGN KEY (statistics_id)
+        REFERENCES statistics (statistics_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 CREATE TABLE IF NOT EXISTS duplications (
-  duplicate_id int(11) NOT NULL,
-  duplicate varchar(128) DEFAULT NULL,
-  quantity int(11) DEFAULT NULL,
-  line_id int(11) NOT NULL,
-  PRIMARY KEY (duplicate_id, line_id)
+                                            duplicate_id int(11) NOT NULL AUTO_INCREMENT,
+                                            duplicate varchar(128) DEFAULT NULL,
+                                            quantity int(11) DEFAULT NULL,
+                                            line_id int(11) NOT NULL,
+                                            PRIMARY KEY (duplicate_id, line_id)
 )
-ENGINE = INNODB,
-CHARACTER SET utf8,
-COLLATE utf8_unicode_ci;
+    ENGINE = INNODB,
+    CHARACTER SET utf8,
+    COLLATE utf8_unicode_ci;
 
 ALTER TABLE duplications
-ADD INDEX `statistics-duplicates_idx` (duplicate_id);
+    ADD INDEX `statistics-duplicates_idx` (duplicate_id);
 
 ALTER TABLE duplications
-ADD CONSTRAINT `statistics-duplicates_fk` FOREIGN KEY (duplicate_id)
-REFERENCES statistics (statistics_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+    ADD CONSTRAINT `statistics-duplicates_fk` FOREIGN KEY (duplicate_id)
+        REFERENCES statistics (statistics_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
